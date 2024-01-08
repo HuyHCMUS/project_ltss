55361910e83913929943c243101a27bf5c89c842
Le Xuan Huy <lexuanhuya5@gmail.com>
2024-01-08 10:49:45 +0700

    bo timing

--- demo.cc
+++ demo.cc
@@ -29,15 +29,6 @@
 #include "src/optimizer.h"
 #include "src/optimizer/sgd.h"
 
-void printTiming(std::vector<float> &timings) {
-  for (int i = 0; i < timings.size(); i ++) {
-    std::cout
-      << "L" << i
-      << " (" << timings[i] << "s)"
-      << (i < timings.size() - 1 ? " -> " : "\n");
-  }
-}
-
 Layer* make_conv_layer(char* device, int channel_in, int height_in, int width_in, int channel_out, int height_kernel, int width_kernel, int stride = 1, int pad_w = 0, int pad_h = 0) {
   if (strcmp(device, "cpu") == 0) {
     return (Layer *)new Conv(channel_in, height_in, width_in, channel_out, height_kernel, width_kernel, stride, pad_w, pad_h);
@@ -177,8 +168,6 @@ int main(int argc, char* argv[]) {
       // display
       if (ith_batch % 50 == 0 && ith_batch > 0) {
         std::cout << ith_batch << "-th batch, loss: " << dnn.get_loss() << std::endl;
-        printTiming(forward_timings);
-        forward_timings = std::vector<float>(dnn.num_layers(), 0);
       }
       // optimize
       dnn.update(opt);
